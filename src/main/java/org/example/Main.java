b211a35012b90841ca99c57cad4d19c45a77483d
Ilya Sevastyanov <sevastyanov-1990@bk.ru.>
2024-02-17 16:15:50 +0300

    Start project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import domen.Product;
+
+public class Main {
+    public static void main(String[] args)  {
+        Product product = new Product(1,"cheaps Lays", 132.55,13);
+        System.out.println("product = " + product);
+    }
+}
---
+++ src/main/java/services/VendingMachine.java
@@ -0,0 +1,13 @@
+package services;
+
+import domen.Product;
+
+import java.util.List;
+
+public class VendingMachine {
+    private Holder holder;
+    private Display display;
+    private CoinDespender coinDespender;
+    private List<Product> products;
+    
+}
