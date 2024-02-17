4d110d03dc004b59afb888ac6b73185d3116feef
Ilya Sevastyanov <sevastyanov-1990@bk.ru.>
2024-02-17 16:43:51 +0300

    Создание дополнительной ветки

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,32 @@
 package org.example;
 
+import domen.Bottle;
 import domen.Product;
+import services.CoinDespender;
+import services.Display;
+import services.Holder;
+import services.VendingMachine;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args)  {
-        Product product = new Product(1,"cheaps Lays", 132.55,13);
-        System.out.println("product = " + product);
+    public static void main(String[] args) {
+        Product product1 = new Product(1, "cheaps Lays", 132.55, 13);
+        Product product2 = new Product(2, "chocolate", 55.23, 10);
+        Product product3 = new Product(3, "nuts", 222.55, 15);
+        Product bottle1 = new Bottle(4, "Cola", 13.22, 24, 0.5);
+        List<Product> ourlist = new ArrayList<>();
+        ourlist.add(product1);
+        ourlist.add(product2);
+        ourlist.add(product3);
+        ourlist.add(bottle1);
+        Holder holder = new Holder();
+        Display display = new Display();
+        CoinDespender despender = new CoinDespender();
+        VendingMachine machine = new VendingMachine(holder, display, despender, ourlist);
+        for (Product elem : machine.getProducts()) {
+            System.out.println(elem);
+        }
     }
 }
--- src/main/java/services/VendingMachine.java
+++ src/main/java/services/VendingMachine.java
@@ -9,5 +9,64 @@ public class VendingMachine {
     private Display display;
     private CoinDespender coinDespender;
     private List<Product> products;
-    
+
+    public VendingMachine(Holder holder, Display display, CoinDespender coinDespender, List<Product> products) {
+        this.holder = holder;
+        this.display = display;
+        this.coinDespender = coinDespender;
+        this.products = products;
+    }
+
+    public Holder getHolder() {
+        return holder;
+    }
+
+    public void setHolder(Holder holder) {
+        this.holder = holder;
+    }
+
+    public Display getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(Display display) {
+        this.display = display;
+    }
+
+    public CoinDespender getCoinDespender() {
+        return coinDespender;
+    }
+
+    public void setCoinDespender(CoinDespender coinDespender) {
+        this.coinDespender = coinDespender;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "VendingMachine{" +
+                "holder=" + holder +
+                ", display=" + display +
+                ", coinDespender=" + coinDespender +
+                ", products=" + products +
+                '}';
+    }
+
+    /**
+     * @param product обьект добавляемого продукта
+     * @apiNote добавление продукта в аппарат
+     */
+    public void addProduct(Product product) {
+        if (product.getPrice() <= 0) {
+            product.setPrice(100);
+        }
+        this.products.add(product);
+    }
 }
